825999285a282426df6b184e15f6921ba4adc27b
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-03-07 21:41:22 -0800

    fix reset board bug

--- app.js
+++ app.js
@@ -41,5 +41,5 @@ function changeColor(choice) {
 function resetBoard() {
   let grid = document.querySelector("#etch-grid");
   let squares = grid.querySelectorAll("div");
-  squares.forEach((div) => div.remove());
+  squares.forEach((div) => (div.style.backgroundColor = "white"));
 }
