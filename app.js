665c896f8f30a4265650381aba9e96b2849d8b14
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-03-07 21:40:17 -0800

    add random and reset button functionality

--- app.js
+++ app.js
@@ -27,9 +27,19 @@ function changeSize(input) {
 }
 
 function colorSquare() {
-  this.style.backgroundColor = color;
+  if (color === "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
 }
 
 function changeColor(choice) {
   color = choice;
+}
+
+function resetBoard() {
+  let grid = document.querySelector("#etch-grid");
+  let squares = grid.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
 }
